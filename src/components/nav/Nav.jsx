4ae8f483db8903f65346a7ca727a0e96063675b0
KristinaSolo <97984146+KristinaSolo@users.noreply.github.com>
2022-08-22 16:10:27 +0300

    0987654

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './nav.scss'
+
+ function Nav() {
+  return (
+    <div className='nav'>
+    <a href='#' className='nav__links'>Prpgress</a>
+    <a href='#' className='nav__links'>Lod In</a>
+      
+    </div>
+  )
+};
+
+export default Nav
